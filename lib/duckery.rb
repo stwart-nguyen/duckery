759861e7782420dfe80e5135949ef393c263045e
stewart <npcuong.011308@gmail.com>
2020-04-01 22:18:28 +0700

    Allow to check duck variants

--- lib/duckery.rb
+++ lib/duckery.rb
@@ -1,9 +1,35 @@
 module Duckery
+  def self.prepended(base)
+    base.singleton_class.prepend(@class_methods_module)
+  end
+
   def self.configuration
-    @configuration ||= Configuration.new
+    @configuration ||= Duckery::Configuration.new
   end
 
   def self.config
+    reset_configuration
+
     yield(configuration) if block_given?
+
+    @class_methods_module = Module.new do
+      Duckery.configuration.variants.each do |variant|
+        Duckery.variants_count[variant] = []
+
+        define_method variant do |*args|
+          Duckery.variants_count[variant].push(name) # Roll call
+
+          super(*args)
+        end
+      end
+    end
+  end
+
+  def self.reset_configuration
+    @configuration = Duckery::Configuration.new
+  end
+
+  def self.variants_count
+    @variants_count ||= {}
   end
 end
