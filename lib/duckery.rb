b2c048f9357d9e20a121253a6a9d1804d82c7376
stewart <npcuong.011308@gmail.com>
2020-05-09 11:31:48 +0700

    Update prepending strategy

--- lib/duckery.rb
+++ lib/duckery.rb
@@ -2,34 +2,40 @@
 
 require 'configuration'
 require 'duckery/railtie' if defined?(Rails)
+require 'byebug'
 
 module Duckery
   def self.prepended(base)
-    base.singleton_class.prepend(@class_methods_module)
+    overiden_classes_module =
+      Module.new do
+        Duckery.configuration.variants.each do |variant|
+          Duckery.statistic[base.name] ||= { variants: [] }
+
+          # Lookup on self and ancestor for method `variant`
+          next unless base.methods.include?(variant)
+
+          define_method variant do |*args|
+            Duckery.statistic[base.name][:variants].push(variant) # Roll call
+
+            super(*args)
+          end
+        end
+      end
+
+    base.singleton_class.prepend(overiden_classes_module)
   end
 
   def self.configuration
     @configuration ||= Duckery::Configuration.new
   end
 
+  # TODO: convert to symbols
+  def self.variants
+    configuration.variants
+  end
+
   def self.config
-    reset_configuration
-
     yield(configuration) if block_given?
-
-    @class_methods_module = Module.new do
-      Duckery.configuration.variants.each do |variant|
-        define_method variant do |*args|
-          Duckery.statistic[name] ||= { variants: [] }
-
-          return unless defined?(super)
-
-          Duckery.statistic[name][:variants].push(variant) # Roll call
-
-          super(*args)
-        end
-      end
-    end
   end
 
   def self.reset_configuration
